91c9dd0935643169a1e0f347a6a12b5bedbc21aa
MarDuo2022 <113748980+MarDuo2022@users.noreply.github.com>
2023-01-07 22:19:04 +0800

    reverse bar chart order

--- static/js/app.js
+++ static/js/app.js
@@ -12,33 +12,31 @@ console.log("Data Promise: ", dataPromise);
 d3.json(url).then(function(data) {
     console.log(data)
 
+    let samples = data.samples;
+    let samplesPropertiesArray = samples.filter(sample => sample.id == 940);
+    let samplesProperties = samplesPropertiesArray[0];
 
-let samples = data.samples;
-let samplesPropertiesArray = samples.filter(sample => sample.id == 940);
-let samplesProperties = samplesPropertiesArray[0];
+    let otu_ids = samplesProperties.otu_ids;
+    let otu_labels = samplesProperties.otu_labels;
+    let sample_values = samplesProperties.sample_values;
 
-let otu_ids = samplesProperties.otu_ids;
-let otu_labels = samplesProperties.otu_labels;
-let sample_values = samplesProperties.sample_values;
+    let yvalues = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`).reverse();
 
-let yvalues = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`);
+    // Collate data for bar
+    let barData = {
+        x: sample_values.slice(0,10).reverse(),
+        y: yvalues,
+        type: 'bar',
+        text: otu_labels.slice(0,10).reverse(),
+        orientation: 'h'
+    };
 
-// Collate data for bar
-let barData = {
-    x: sample_values.slice(0,10),
-    y: yvalues,
-    type: 'bar',
-    text: otu_labels.slice(0,10),
-    orientation: 'h'
-};
+    let barArray = [barData];
 
-let barArray = [barData];
+    let barLayout = {
+        title: "Top 10 OTUs",
+        margin: {t: 40, l: 100}
+    };
 
-let barLayout = {
-    title: "Top 10 OTUs",
-    margin: {t: 40, l: 100}
-};
-
-Plotly.newPlot('bar', barArray, barLayout)
-
+    Plotly.newPlot('bar', barArray, barLayout)
 });
