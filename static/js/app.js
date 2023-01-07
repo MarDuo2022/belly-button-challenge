af7df0d8f6948b7413748c7da2a172217058d3da
MarDuo2022 <113748980+MarDuo2022@users.noreply.github.com>
2023-01-07 22:17:37 +0800

    finished 2. bar graph

--- static/js/app.js
+++ static/js/app.js
@@ -7,7 +7,38 @@ const dataPromise = d3.json(url);
 console.log("Data Promise: ", dataPromise);
 
 // Fetch the JSON data and console log it
+
+// fetch JSON data has to be done together with plot 
 d3.json(url).then(function(data) {
     console.log(data)
-});
 
+
+let samples = data.samples;
+let samplesPropertiesArray = samples.filter(sample => sample.id == 940);
+let samplesProperties = samplesPropertiesArray[0];
+
+let otu_ids = samplesProperties.otu_ids;
+let otu_labels = samplesProperties.otu_labels;
+let sample_values = samplesProperties.sample_values;
+
+let yvalues = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`);
+
+// Collate data for bar
+let barData = {
+    x: sample_values.slice(0,10),
+    y: yvalues,
+    type: 'bar',
+    text: otu_labels.slice(0,10),
+    orientation: 'h'
+};
+
+let barArray = [barData];
+
+let barLayout = {
+    title: "Top 10 OTUs",
+    margin: {t: 40, l: 100}
+};
+
+Plotly.newPlot('bar', barArray, barLayout)
+
+});
